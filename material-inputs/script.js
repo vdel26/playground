d71e780b3d766dcf77d693c5bc9565f426569aeb
Victor Delgado <vdel26@gmail.com>
2014-11-27 01:37:05 +0100

    click transition on material-design

--- material-inputs/script.js
+++ material-inputs/script.js
@@ -7,17 +7,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
   input.addEventListener('focus', function () {
     input.parentNode.classList.add('is-focused');
+    label.classList.add('is-minimized');
   });
 
   input.addEventListener('blur', function () {
     input.parentNode.classList.remove('is-focused');
+    label.classList.remove('is-minimized');
   });
 
   input.addEventListener('keyup', function () {
     if (!input.validity.valid) input.parentNode.classList.add('is-invalid');
     else input.parentNode.classList.remove('is-invalid');
-
-    if (input.value !== '') label.classList.add('is-minimized');
-    else label.classList.remove('is-minimized');
   });
 });
